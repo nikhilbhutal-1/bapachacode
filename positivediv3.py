57afa2f15c3ef78326ed1ae3cdb77df8124dbadc
nikhilbhutal-1 <nickgamer1980@gmail.com>
2025-06-26 10:41:05 +0530

    best way of finding div3 even and positive

--- positivediv3.py
+++ positivediv3.py
@@ -1,13 +1,17 @@
 def div3():
     num = int(input("Enter a number: "))
-    if  num > 0 and num % 2 == 0:
-        return "number is even and positive number"
-    elif num < 0 :
-        return "number is negative number"
-    elif num % 3 == 0 :
+    if num == 0:
+        return "zero"
+    if  num > 0 and num % 2 == 0 and num % 3 == 0:
+        return "number is positive and even and divisible by 3"
+    elif num > 0 and num % 2 == 0:
+        return "number is positive and even"
+    elif num > 0 and num % 3 == 0:
         return "number is divisible by 3 and positive number"
-    elif num == 0 :
-        return"zero"
+    elif num < 0:
+        return "number is negative"
+    else:
+        return"invalid number"
 
 def main():
     print(div3())
