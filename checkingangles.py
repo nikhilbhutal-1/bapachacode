960e799a16e68f64ab887249f1e609dfdff744fb
nikhilbhutal-1 <nickgamer1980@gmail.com>
2025-06-26 10:53:11 +0530

    agles checker

--- checkingangles.py
+++ checkingangles.py
@@ -1,2 +1,19 @@
 def angles():
-    num = int(input()
+    num = int(input(" enter angle :"))
+    if num < 90 :
+        return "acute"
+    elif num == 90 :
+        return "right"
+    elif num > 90 and num < 180 :
+        return "obtuse"
+    elif num == 180:
+        return "straight"
+    elif num > 180 and num < 360:
+        return "reflex"
+    else:
+        return "invalid"  
+    
+def main():
+    print(angles())
+
+main()        
