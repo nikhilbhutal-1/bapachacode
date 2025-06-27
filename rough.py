9a2d295102ae72a8d15a2ac5f600cd76eeb14785
nikhilbhutal-1 <nickgamer1980@gmail.com>
2025-06-27 20:47:00 +0530

    rough file added

---
+++ rough.py
@@ -0,0 +1,18 @@
+# def greet(name):
+#     return f"hello , {name}"
+# def square(x):
+#     return x*x 
+# def outer():
+#     def inner():
+#         return "hello"
+#     return inner()
+
+# # print(greet("nikhil"))
+# # print(square(7))  # Output: 
+# print(outer())
+# def greet(name = "stranger"):
+#     return f"Hello, {name}"
+# print(greet())  
+student = [ 49, 47, 89, 95, 68]
+student.insert( 5 , 80 )
+print(student)  
